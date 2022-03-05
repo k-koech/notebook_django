e17857deefd2182b909d1f0f5dda24a44c1a51d7
k-koech <kelvin.koech@student.moringaschool.com>
2022-03-05 18:44:14 +0300

    styled the dshboard

--- notes/views/notes.py
+++ notes/views/notes.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+from notes.models import Notes
 
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    notes = Notes.objects.filter(user=request.user)
+    print(notes)
+    return render(request, "dashboard.html",{"notes":notes})
 
 def add_notes(request):
-    return render(request, "add_notes.html")
+    if request.method=="POST":
+        title=request.POST.get('title')
+        notes=request.POST.get('description')
+        note = Notes(title=title, notes=notes, user=request.user)
+        note.save()                             
+        messages.add_message(request, messages.SUCCESS, 'Registered successfully')
+        return redirect(dashboard)
+
+    else:
+        return render(request, "add_notes.html")
+
+def update_notes(request, id):
+    if request.method=="POST":
+        title=request.POST.get('title')
+        notes=request.POST.get('description')
+
+        note = Notes()
+        note.title=title
+        note.notes=notes
+        note.user=request.user
+        note.save()                             
+        messages.add_message(request, messages.SUCCESS, 'Registered successfully')
+        return redirect(dashboard)
+
+    else:
+
+        return render(request, "update_notes.html")
