5a76f056a4287b4273cb2ec276d224c10044bee9
k-koech <kelvin.koech@student.moringaschool.com>
2022-03-07 01:35:38 +0300

    Added sweet alert for profilephoto update

--- notebook/settings.py
+++ notebook/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes'
+    'notes',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- notes/views/auth.py
+++ notes/views/auth.py
@@ -130,7 +130,7 @@ def profile(request):
                 user=Users.objects.get(id=request.user.id)
                 user.profile_photo=profile_photo
                 user.save()
-                messages.add_message(request,messages.SUCCESS,"Profile photo updated successfully!")
+                # messages.add_message(request,messages.SUCCESS,"Profile photo updated successfully!")
                 return redirect(profile)
 
     else:
